c541859850c2a94bae16cc4bcbaf51a787342b98
Dhevesh <dheveshp@gmail.com>
2019-04-22 12:41:56 +0100

    Initilal Commit - Connecting project to repository

---
+++ LoginForm/LoginForm/LoginForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace LoginForm
+{
+    partial class Form_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_Submit = new System.Windows.Forms.Button();
+            this.TxtBox_Username = new System.Windows.Forms.TextBox();
+            this.TxtBox_Pword = new System.Windows.Forms.TextBox();
+            this.Lbl_Username = new System.Windows.Forms.Label();
+            this.Lbl_Pword = new System.Windows.Forms.Label();
+            this.Lbl_ForgotPassword = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // Btn_Submit
+            // 
+            this.Btn_Submit.Location = new System.Drawing.Point(370, 480);
+            this.Btn_Submit.Margin = new System.Windows.Forms.Padding(6);
+            this.Btn_Submit.Name = "Btn_Submit";
+            this.Btn_Submit.Size = new System.Drawing.Size(150, 44);
+            this.Btn_Submit.TabIndex = 2;
+            this.Btn_Submit.Text = "Submit";
+            this.Btn_Submit.UseVisualStyleBackColor = true;
+            // 
+            // TxtBox_Username
+            // 
+            this.TxtBox_Username.Location = new System.Drawing.Point(370, 246);
+            this.TxtBox_Username.Margin = new System.Windows.Forms.Padding(6);
+            this.TxtBox_Username.Name = "TxtBox_Username";
+            this.TxtBox_Username.Size = new System.Drawing.Size(196, 31);
+            this.TxtBox_Username.TabIndex = 0;
+            // 
+            // TxtBox_Pword
+            // 
+            this.TxtBox_Pword.Location = new System.Drawing.Point(370, 377);
+            this.TxtBox_Pword.Margin = new System.Windows.Forms.Padding(6);
+            this.TxtBox_Pword.Name = "TxtBox_Pword";
+            this.TxtBox_Pword.Size = new System.Drawing.Size(196, 31);
+            this.TxtBox_Pword.TabIndex = 1;
+            this.TxtBox_Pword.UseSystemPasswordChar = true;
+            // 
+            // Lbl_Username
+            // 
+            this.Lbl_Username.AutoSize = true;
+            this.Lbl_Username.Location = new System.Drawing.Point(365, 202);
+            this.Lbl_Username.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.Lbl_Username.Name = "Lbl_Username";
+            this.Lbl_Username.Size = new System.Drawing.Size(110, 25);
+            this.Lbl_Username.TabIndex = 3;
+            this.Lbl_Username.Text = "Username";
+            // 
+            // Lbl_Pword
+            // 
+            this.Lbl_Pword.AutoSize = true;
+            this.Lbl_Pword.Location = new System.Drawing.Point(365, 334);
+            this.Lbl_Pword.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.Lbl_Pword.Name = "Lbl_Pword";
+            this.Lbl_Pword.Size = new System.Drawing.Size(106, 25);
+            this.Lbl_Pword.TabIndex = 4;
+            this.Lbl_Pword.Text = "Password";
+            // 
+            // Lbl_ForgotPassword
+            // 
+            this.Lbl_ForgotPassword.AutoSize = true;
+            this.Lbl_ForgotPassword.Location = new System.Drawing.Point(365, 431);
+            this.Lbl_ForgotPassword.Name = "Lbl_ForgotPassword";
+            this.Lbl_ForgotPassword.Size = new System.Drawing.Size(174, 25);
+            this.Lbl_ForgotPassword.TabIndex = 5;
+            this.Lbl_ForgotPassword.TabStop = true;
+            this.Lbl_ForgotPassword.Text = "Forgot Password";
+            // 
+            // Form_Login
+            // 
+            this.AcceptButton = this.Btn_Submit;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 748);
+            this.Controls.Add(this.Lbl_ForgotPassword);
+            this.Controls.Add(this.Lbl_Pword);
+            this.Controls.Add(this.Lbl_Username);
+            this.Controls.Add(this.TxtBox_Pword);
+            this.Controls.Add(this.TxtBox_Username);
+            this.Controls.Add(this.Btn_Submit);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form_Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_Submit;
+        private System.Windows.Forms.TextBox TxtBox_Username;
+        private System.Windows.Forms.TextBox TxtBox_Pword;
+        private System.Windows.Forms.Label Lbl_Username;
+        private System.Windows.Forms.Label Lbl_Pword;
+        private System.Windows.Forms.LinkLabel Lbl_ForgotPassword;
+    }
+}
+
---
+++ LoginForm/LoginForm/LoginForm.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace LoginForm
+{
+    public partial class Form_Login : Form
+    {
+        public Form_Login()
+        {
+            InitializeComponent();
+            Btn_Submit.Click += new EventHandler(SubmitButton_Click);
+            Log.SetupLogFiles(@"c:\LoginLogs\", "LoginForm", 4);
+        }
+        private readonly string DbConString = ConfigurationManager.ConnectionStrings["employeeDB"].ToString();
+        LoggerDLL.Logger Log = new LoggerDLL.Logger();
+        string userName = "";
+
+        private void Login()
+        {
+            bool isUserFound = false;
+            bool isPasswordFound = false;
+            userName = TxtBox_Username.Text;
+            string password = TxtBox_Pword.Text;
+            int userquery = 0;
+            int passwordquery = 0;
+            using (SqlConnection DbCon = new SqlConnection(DbConString))
+            {
+                SqlCommand usercmd = new SqlCommand();
+                SqlCommand passwordcmd = new SqlCommand();
+                usercmd.Connection = DbCon;
+                passwordcmd.Connection = DbCon;
+                usercmd.CommandText = "SELECT 1 FROM tb_Login WHERE Uname = @Uname";
+                passwordcmd.CommandText = $"SELECT Password FROM tb_Login WHERE Uname = @Uname";
+                usercmd.Parameters.AddWithValue("@Uname", userName);
+                passwordcmd.Parameters.AddWithValue("@Uname", userName);
+                try
+                {
+                    DbCon.Open();
+                    if (Convert.ToInt16(usercmd.ExecuteScalar()) == 1)
+                    {
+                        if (Convert.ToString(passwordcmd.ExecuteScalar()) == password)
+                        {
+                            MessageBox.Show("Login Successful");
+                            SetIsLoggedIn(userName, 1);
+                            SetLoginTime(userName);
+                            ResetFailedLoginAttempts(userName);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Invalid Password");
+                            SetFailedLoginAttempts(userName);
+                            if (getFailedLoginAttempts(userName) == 3)
+                            {
+                                LockAccount(userName);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("User not found!");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Log.Error(e.ToString());
+                }
+                finally
+                {
+                    DbCon.Close();
+                }
+            }
+        }
+
+        private void SetIsLoggedIn(string _userName, int isLoggedIn)
+        {
+            using (var dbCon = new SqlConnection(DbConString))
+            {
+                var dbCmd = new SqlCommand();
+                dbCmd.CommandText = "update tb_Login set isLoggedIn = @isLoggedIn where Uname = @Uname";
+                dbCmd.Parameters.AddWithValue("@isLoggedIn", isLoggedIn);
+                dbCmd.Parameters.AddWithValue("@Uname", _userName);
+                dbCmd.Connection = dbCon;
+
+                try
+                {
+                    dbCon.Open();
+                    dbCmd.ExecuteNonQuery();
+                }
+                catch (Exception e)
+                {
+                    Log.Error(e.ToString());
+                }
+                finally
+                {
+                    dbCon.Close();
+                }
+            }
+        }
+
+        private void SetLoginTime(string _userName)
+        {
+            var LastLoggedInTime = DateTime.Now;
+            using (var dbCon = new SqlConnection(DbConString))
+            {
+                var dbCmd = new SqlCommand();
+                dbCmd.CommandText = "update tb_Login set LastLogInTime = @LastLogInTime where Uname = @Uname";
+                dbCmd.Parameters.AddWithValue("@LastLogInTime", LastLoggedInTime);
+                dbCmd.Parameters.AddWithValue("@Uname", _userName);
+                dbCmd.Connection = dbCon;
+                try
+                {
+                    dbCon.Open();
+                    dbCmd.ExecuteNonQuery();
+                }
+                catch(Exception e)
+                {
+                    Log.Error(e.ToString());
+                }
+                finally
+                {
+                    dbCon.Close();
+                }
+            }
+        }
+
+        private int getFailedLoginAttempts(string _userName)
+        {
+            int getLoggedInAttempts = 0;
+            using (var dbCon = new SqlConnection(DbConString))
+            {
+                var dbCmd = new SqlCommand();
+                dbCmd.CommandText = "select FailedLoginAttempts from tb_Login where Uname = @Uname";
+                dbCmd.Parameters.AddWithValue("@Uname", _userName);
+                dbCmd.Connection = dbCon;
+
+                try
+                {
+                    dbCon.Open();
+                    getLoggedInAttempts = Convert.ToInt16(dbCmd.ExecuteScalar());
+                }
+                catch (Exception e)
+                {
+                    Log.Error(e.ToString());
+                }
+                finally
+                {
+                    dbCon.Close();
+                }
+            }
+            return getLoggedInAttempts;
+        }
+
+        private void SetFailedLoginAttempts(string _userName)
+        {
+            int getFailedLoginAttemptCount = getFailedLoginAttempts(_userName);
+            if (getFailedLoginAttemptCount < 3)
+            {
+                getFailedLoginAttemptCount += 1;
+                using (var dbCon = new SqlConnection(DbConString))
+                {
+                    var dbCmd = new SqlCommand();
+                    dbCmd.CommandText = "update tb_Login set FailedLoginAttempts = @FailedLoginAttempts where Uname = @Uname";
+                    dbCmd.Parameters.AddWithValue("@FailedLoginAttempts", getFailedLoginAttemptCount);
+                    dbCmd.Parameters.AddWithValue("@Uname", _userName);
+                    dbCmd.Connection = dbCon;
+
+                    try
+                    {
+                        dbCon.Open();
+                        dbCmd.ExecuteNonQuery();
+                    }
+                    catch(Exception e)
+                    {
+                        Log.Error(e.ToString());
+                    }
+                    finally
+                    {
+                        dbCon.Close();
+                    }
+                }
+            }
+        }
+
+        private void LockAccount(string _userName) //TODO set to public to be used by the admin form
+        {
+            if (getFailedLoginAttempts(_userName) >= 3)
+            {
+                using (var dbCon = new SqlConnection(DbConString))
+                {
+                    var dbCmd = new SqlCommand();
+                    dbCmd.CommandText = "update tb_Login set isLocked = 1 where Uname = @Uname";
+                    dbCmd.Parameters.AddWithValue("@Uname", _userName);
+                    dbCmd.Connection = dbCon;
+
+                    try
+                    {
+                        dbCon.Open();
+                        dbCmd.ExecuteNonQuery();
+                    }
+                    catch (Exception e)
+                    {
+                        Log.Error(e.ToString());
+                    }
+                    finally
+                    {
+                        dbCon.Close();
+                    }
+                }
+            }
+        }
+
+        private void ResetFailedLoginAttempts(string _userName) //TODO set to public to be used by the admin form
+        {
+            using (var dbCon = new SqlConnection(DbConString))
+            {
+                var dbCmd = new SqlCommand();
+                dbCmd.CommandText = @"update tb_Login set FailedLogInAttempts = 0 where Uname = @Uname";
+                dbCmd.Parameters.AddWithValue("@Uname", _userName);
+                dbCmd.Connection = dbCon;
+
+                try
+                {
+                    dbCon.Open();
+                    dbCmd.ExecuteNonQuery();
+                }
+                catch(Exception e)
+                {
+                    Log.Error(e.ToString());
+                }
+                finally
+                {
+                    dbCon.Close();
+                }
+            }
+        }
+
+        public void SubmitButton_Click(object sender, EventArgs e)
+        {
+            Login();
+        }
+    }
+}
